d943aac4ee307ba33da3bd12f679efe292dcc7ec
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2022-10-28 16:39:53 +0100

    Update read_imagenet_64X64.py

--- read_imagenet_64X64.py
+++ read_imagenet_64X64.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 def unpickle(file):
     with open(file, 'rb') as fo:
@@ -26,23 +27,19 @@ def load_databatch( img_size=64):
     Y_train = y[0:data_size]
     
     return X_train,Y_train
-
-
     
 X_train,Y_train=load_databatch()
 X_train=X_train.transpose(0,2,3,1)
 
-print(X_train.shape)
-print(len(Y_train))
+# print(X_train.shape)
+# print(len(Y_train))
 
-
-x1=X_train[200,:,:,:]
-y1=Y_train[200]
+x1=X_train[20,:,:,:]
+y1=Y_train[20]
 
 with open(r'C:\My_Data\trasnformer_code\imagenet_classes.txt') as f:
     labels = [line.strip() for line in f.readlines()]
-    
+
 plt.figure()
 plt.imshow(x1)
-
 print(labels[y1])
